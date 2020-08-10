a6d2ac9daf8d33745e7086d0704e3015383448b9
abdiahmedxd <abdiahmedxd@gmail.com>
2020-08-10 08:49:37 +0700

    project toko online

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerSlide />
+    <GaleryShayna />
+    <LogoShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import BannerSlide from "@/components/BannerSlide.vue";
+import GaleryShayna from "@/components/GaleryShayna.vue";
+import LogoShayna from "@/components/LogoShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+
+export default {
+  name: "home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    BannerSlide,
+    GaleryShayna,
+    LogoShayna,
+    FooterShayna
+
+  }
+}
+</script>
